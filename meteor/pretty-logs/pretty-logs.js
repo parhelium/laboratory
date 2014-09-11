55dc92ca4051f42a9190cc9a2af64ae0b9ca0bd3
parhelium <me@parhelium.pl>
2014-09-11 09:26:09 +0200

    Pretty-logs : testing why `chalk.enabled = true` is necessary to make chalk working properly with meteor

--- meteor/pretty-logs/pretty-logs.js
+++ meteor/pretty-logs/pretty-logs.js
@@ -5,6 +5,7 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
     Meteor.startup(function () {
         var chalk = Meteor.require('chalk');
+        console.log("chalk.supportsColor -> ",chalk.supportsColor);
         chalk.enabled = true;
         console.log( chalk.red('AAAAAAAAAAAAAAAAAA' ))
     });
