b716b15e4c3affea7c2b4d1ffa52172354d09889
parhelium <me@parhelium.pl>
2014-09-25 12:56:02 +0200

    meteor : ractive-integration : catch errors

--- meteor/ractive-integration/ractive-integration.js
+++ meteor/ractive-integration/ractive-integration.js
@@ -9,10 +9,17 @@ if (Meteor.isClient) {
         }else{
             console.log("Ractive is installed " ,Ractive);
         }
+        try{
+            var template = this.$("rasct")
+                .contents()
+                .filter(function() {
+                    return this.nodeType === 8;
+                })
+                .get(0).nodeValue;
+        }catch(error){
+            console.error("Ractive template was not properly defined. Wrap it with:\n<ract><!--\nRACTIVE_TEMPLATE\n--></ract>")
+        }
 
-        var template = this.$("ract").contents().filter(function() {
-                return this.nodeType === 8;
-            }).get(0).nodeValue;
 
         var ractive = new Ractive({
             el: ".container",
