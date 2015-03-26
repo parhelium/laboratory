e4d1bbe71d6928c8b70aa95ccd5d0e2d7aa5d3ba
Kuba Wyrobek <kuba.wyrobek@gmail.com>
2015-03-26 12:14:09 +0100

    the simplest possible test update

--- meteor/gagarin/pure/tests/gagarin/simplest.js
+++ meteor/gagarin/pure/tests/gagarin/simplest.js
@@ -1,6 +1,14 @@
 describe('Example test suite', function () {
     var server = meteor();
+
     it('execute should work', function () {
-        expect(null).to.be.null;
+        // return a promise
+        return server.execute(function () {
+            return Meteor.release;
+        })
+            .then(function (value) {
+                expect(value).not.to.be.empty;
+            });
     });
+
 });
